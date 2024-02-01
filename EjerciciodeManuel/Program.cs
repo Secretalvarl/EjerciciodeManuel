1f7c18ac7f6b0be70c632a4bb6f40f774c219690
DESKTOP-R8KP20E\Profesor <csi23-agarera@colegioaltair.net>
2024-02-01 13:37:21 +0100

    Agregar archivos de proyecto.

---
+++ EjerciciodeManuel/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Runtime.Serialization.Formatters;
+
+namespace EjerciciodeManuel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random numeroRandom = new Random();
+
+            int longitudArray = numeroRandom.Next(10, 20);
+
+            int[] arrayNumeros = new int[longitudArray];
+
+            for (int i = 0; i < arrayNumeros.Length - 1; i++)
+            {
+                arrayNumeros[i] = numeroRandom.Next(1, 200);
+            }
+
+            Console.WriteLine("Escriba el último dígito para completar el array: ");
+            string ultimoNumero = Console.ReadLine();
+
+            if (int.TryParse(ultimoNumero, out int numero))
+            {
+                arrayNumeros[arrayNumeros.Length - 1] = numero;
+
+                
+                Console.WriteLine("Array completo:");
+                foreach (var num in arrayNumeros)
+                {
+                    Console.Write(num + " ");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error-El dato introducido no es un numero");
+            }
+
+            Array.Sort(arrayNumeros);
+            Array.Reverse(arrayNumeros);
+
+            Console.WriteLine("\n" + "Array completo (ordenado de forma descendente):");
+            foreach (var num in arrayNumeros)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
